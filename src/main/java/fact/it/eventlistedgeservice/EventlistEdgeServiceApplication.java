f54fed94d3900d6a61e810fc029b2cfae4039ce1
Thijs <r0793167@student.thomasmore.be>
2021-12-12 17:27:43 +0100

    Init project

---
+++ src/main/java/fact/it/eventlistedgeservice/EventlistEdgeServiceApplication.java
@@ -0,0 +1,16 @@
+package fact.it.eventlistedgeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class EventlistEdgeServiceApplication {
+    @Bean
+    public RestTemplate getRestTemplate(){ return new RestTemplate(); }
+    public static void main(String[] args) {
+        SpringApplication.run(EventlistEdgeServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fact/it/eventlistedgeservice/EventlistEdgeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fact.it.eventlistedgeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventlistEdgeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
