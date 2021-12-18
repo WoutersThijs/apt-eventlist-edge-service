4d7d7e5497ddb89d83f7a842c24e89a0e2cd0090
Thijs <r0793167@student.thomasmore.be>
2021-12-18 15:51:13 +0100

    Artist model GET SET

--- src/main/java/fact/it/eventlistedgeservice/model/Artist.java
+++ src/main/java/fact/it/eventlistedgeservice/model/Artist.java
@@ -6,4 +6,44 @@ public class Artist {
     private String artist;
     private Integer hour;
     private Integer minute;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEvent() {
+        return event;
+    }
+
+    public void setEvent(String event) {
+        this.event = event;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public Integer getHour() {
+        return hour;
+    }
+
+    public void setHour(Integer hour) {
+        this.hour = hour;
+    }
+
+    public Integer getMinute() {
+        return minute;
+    }
+
+    public void setMinute(Integer minute) {
+        this.minute = minute;
+    }
 }
