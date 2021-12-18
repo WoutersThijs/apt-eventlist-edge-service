3cba59ab3f40d0c8f90dedd7f9548f6669931158
Thijs <r0793167@student.thomasmore.be>
2021-12-18 15:52:38 +0100

    Event model GET SET

--- src/main/java/fact/it/eventlistedgeservice/model/Event.java
+++ src/main/java/fact/it/eventlistedgeservice/model/Event.java
@@ -4,4 +4,28 @@ public class Event {
     private String id;
     private String eventName;
     private String organiser;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public String getOrganiser() {
+        return organiser;
+    }
+
+    public void setOrganiser(String organiser) {
+        this.organiser = organiser;
+    }
 }
