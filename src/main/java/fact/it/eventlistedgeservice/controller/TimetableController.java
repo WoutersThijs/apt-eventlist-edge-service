af47a2af82486b9ab740300329f0a64cb3dc317a
Thijs <r0793167@student.thomasmore.be>
2021-12-29 17:30:21 +0100

    GET eventlists/event/eventName

--- src/main/java/fact/it/eventlistedgeservice/controller/TimetableController.java
+++ src/main/java/fact/it/eventlistedgeservice/controller/TimetableController.java
@@ -1,9 +1,21 @@
 package fact.it.eventlistedgeservice.controller;
 
+import fact.it.eventlistedgeservice.model.Artist;
+import fact.it.eventlistedgeservice.model.Event;
+import fact.it.eventlistedgeservice.model.FilledEventArtist;
+import fact.it.eventlistedgeservice.model.Timetable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class TimetableController {
@@ -16,4 +28,29 @@ public class TimetableController {
 
     @Value("${artistservice.baseurl}")
     private String artistServiceBaseUrl;
+
+    @GetMapping("/eventlists")
+    public List<FilledEventArtist> getEventList(){
+        List<FilledEventArtist> returnList = new ArrayList<>();
+
+        ResponseEntity<List<Event>> responseEntityEvents =
+                restTemplate.exchange("http://" + eventServiceBaseUrl + "/events",
+                        HttpMethod.GET, null, new ParameterizedTypeReference<List<Event>>() {
+                        });
+
+        List<Event> events = responseEntityEvents.getBody();
+
+        for(Event event : events){
+
+        }
+        return null;
+    }
+
+    @GetMapping("/eventlists/event/{eventName}")
+    public Timetable getEventlistsByEventName(@PathVariable String eventName){
+        Event event = restTemplate.getForObject("http://" + eventServiceBaseUrl + "/events/{eventName}", Event.class, eventName);
+        ResponseEntity<List<Artist>> responseEntityArtists = restTemplate.exchange("http://" + artistServiceBaseUrl + "/artists/event/{eventName}", HttpMethod.GET, null, new ParameterizedTypeReference<List<Artist>>(){}, eventName);
+
+        return new Timetable(event, responseEntityArtists.getBody());
+    }
 }
--- src/main/java/fact/it/eventlistedgeservice/model/Timetable.java
+++ src/main/java/fact/it/eventlistedgeservice/model/Timetable.java
@@ -1,7 +1,51 @@
 package fact.it.eventlistedgeservice.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Timetable {
     private String eventName;
     private String organiser;
-    private String artistName;
+    private List<Artist> artists;
+
+    public Timetable(Event event, List<Artist> tempArtists){
+        setEventName(event.getEventName());
+        setOrganiser(event.getOrganiser());
+
+        artists = new ArrayList<>();
+
+        tempArtists.forEach(artist -> {
+            artists.add(new Artist(
+                    artist.getEvent(),
+                    artist.getArtist(),
+                    artist.getHour(),
+                    artist.getMinute()));
+        });
+
+        setArtists(artists);
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public String getOrganiser() {
+        return organiser;
+    }
+
+    public void setOrganiser(String organiser) {
+        this.organiser = organiser;
+    }
+
+    public List<Artist> getArtists() {
+        return artists;
+    }
+
+    public void setArtists(List<Artist> artists) {
+        this.artists = artists;
+    }
 }
